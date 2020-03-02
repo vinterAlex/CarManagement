9fa046dd798cbbd8b83359b0809d0ccd24c3911e
Alex Vinter <alex.vinter24@gmail.com>
2020-03-02 12:41:01 +0200

    no message

--- CarManagement/Pages/AllCars.cshtml.cs
+++ CarManagement/Pages/AllCars.cshtml.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using CarManagement.Models;
 using VehicleDefinition;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CarManagement
 {
@@ -16,18 +18,52 @@ namespace CarManagement
         //get the connection from "shop" db
         private readonly shopContext _context;
 
+        public IList<Vehicle> VehiclesToShow { get; set; } //to retrieve all cars
+        public IList<string> vehicleItemSearch { get; set; }
+
         public AllCarsModel(shopContext context)
         {
             _context = context;
         }
 
-        public IList<Vehicle> VehiclesToShow { get;set; } //to retrieve all cars
+
+        public List<Vehicle> somedata { get; set; }
+
+
         
 
+        public IList<string> populateFilterItems()
+        {
+            string queryString = "select distinct Make from Vehicles;";
+
+            IList<string> tempList = new List<string>();
+
+            using (SqlConnection connection = new SqlConnection("Server=ALEXV\\TESTINSTANCE;Database=shop;User ID=vinteralex;Password=blestem24;"))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    if (!reader.IsDBNull(0))
+                    {
+                        tempList.Add(reader[0].ToString());
+                    }
+                }
+                reader.Close();
+            }
+            //string[] carName = tempList.ToArray();
+
+
+            vehicleItemSearch = tempList.ToList();
+
+            return vehicleItemSearch;
+        }
 
         public async Task OnGetAsync()
         {
-            
+
             VehiclesToShow = await _context.Vehicles.ToListAsync();
 
         }
--- FilterSearchDefinition/FilterSearch.cs
+++ FilterSearchDefinition/FilterSearch.cs
@@ -1,19 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using VehicleDefinition;
 
 namespace FilterSearchDefinition
 {
-    public class FilterSearch 
+    public class FilterSearch : Vehicle
     {
         public string VehicleDriveTypeFilter  { get; set; }
         public string VehicleEngineDescriptionFilter { get; set; }
         public string VehicleMakeFilters { get; set; }
         public string VehicleModelFilter { get; set; }
         public int VehicleConstructionYearFilter { get; set; }
-        
+
+
+
     }
 }
